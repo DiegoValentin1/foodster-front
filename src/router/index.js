7f48447a14718cd170845b941612a43e6e2e789a
oscarmuguelb <20213tn153@utez.edu.mx>
2024-03-26 22:13:58 -0600

    Guardar cambios locales en src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,11 @@ const router = new VueRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: Profile
     }
   ]
 })
