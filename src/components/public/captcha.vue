b26cb9ddc287ef32d8bca2fe1e143894e1fa4665
CristianRdz <104938066+CristianRdz@users.noreply.github.com>
2024-04-01 12:52:26 -0600

    Se agrego captcha en front

---
+++ src/components/public/captcha.vue
@@ -0,0 +1,79 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <label for="name"> Nombre:</label><br />
+    <input type="text" id="name"
+           v-model="formData.name" required />
+    <br/><br/>
+
+    <div
+        ref="container"
+        class="frc-captcha"
+        data-sitekey="FCMJTM9F6S3ERAC7"
+        data-lang="es"
+    ></div>
+
+    <!-- Botón de envío -->
+    <button type="submit">Enviar</button>
+  </form>
+</template>
+
+
+<script>
+import { WidgetInstance } from "friendly-challenge";
+
+import { ref } from "vue";
+
+import {verificarCaptcha} from "@/services/CaptchaService";
+
+export default {
+  data() {
+    return {
+      container: ref(),
+      widget: ref(),
+      formData: {
+        name: ""
+      },
+    };
+  },
+
+  methods: {
+    //No comunicamos hacia
+    //el back de nuestra aplicación.
+
+    submitForm() {},
+
+    async verifyCaptcha(solution) {
+      let response = await verificarCaptcha(solution);
+      console.log(response);
+    },
+    doneCallback(solution) {
+      this.verifyCaptcha(solution);
+    },
+
+    errorCallback: (err) => {
+      console.log("There was an error when trying to solve the Captcha.");
+      console.log(err);
+    },
+  },
+
+  mounted() {
+    if (this.$refs.container) {
+      this.widget = new WidgetInstance(
+          this.$refs.container, {
+            startMode: "",
+            doneCallback: this.doneCallback,
+            errorCallback: this.errorCallback,
+          }
+      );
+    }
+  },
+
+  beforeDestroy() {
+    if (this.widget) {
+      this.widget.destroy();
+    }
+  },
+};
+</script>
+
+<style></style>
